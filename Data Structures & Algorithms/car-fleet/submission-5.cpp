c4ccd857c796b3b72003b5ecc77896e944eced71
Imad Dar <112022634+imaddar@users.noreply.github.com>
2026-07-31 04:27:25 -0600

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        
+        stack<double> s;
+        vector<pair<int,double>> pos_and_time;
+        for (int i = 0; i < position.size(); i++) {
+            pair<int,double> p = {position[i], (static_cast<double>(target - position[i])) / speed[i]};
+            pos_and_time.push_back(p);
+        }
+
+        sort(pos_and_time.begin(), pos_and_time.end());
+        for (int i = pos_and_time.size() - 1; i >= 0; i--) {
+            // cout << pos_and_time[i].first << " " << pos_and_time[i].second << endl;
+        }
+
+        for (int i = pos_and_time.size() - 1; i >= 0; i--) {
+            if (s.empty()) {
+                s.push(pos_and_time[i].second);
+            }
+            else if (pos_and_time[i].second > s.top()) {
+                s.push(pos_and_time[i].second);
+            }
+            // cout << pos_and_time[i].second << endl;
+            // cout << s.top() << endl;
+        }
+        return s.size();
+    }
+};
